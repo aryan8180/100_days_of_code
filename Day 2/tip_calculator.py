c4414c0991c93e24145222f290e0391729fd0a59
Aryan Sharma <86112346+aryan8180@users.noreply.github.com>
2024-01-02 12:47:27 +0530

    Day 2 of 100DaysOfCode

---
+++ Day 2/tip_calculator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Tip Calculator!")
+total_bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+new_total_bill = float(total_bill + total_bill*tip/100)
+number_of_people = int(input("How many people to split the bill? "))
+split_per_person = (new_total_bill/number_of_people) 
+print(f"Each person should pay: ${split_per_person}")
